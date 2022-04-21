a5d3f933943957bf91c94c139aaff23198c87ff6
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2022-04-22 00:42:27 +0700

    feat: updated color service to use color instead of color builder

--- src/service/color/color.ts
+++ src/service/color/color.ts
@@ -1,13 +1,7 @@
 import { getRandomElement } from '../../utils/random'
-import {
-  AvailableColor,
-  AvailableColorBuilders,
-  Color,
-  ColorBuilder,
-} from '../../model'
+import { AvailableColors, Color } from '../../model'
 
-const getRandomColorBuilder = (): ColorBuilder<AvailableColor> =>
-  getRandomElement(AvailableColorBuilders)
+const getRandomColor = () => new (getRandomElement(AvailableColors))()
 
 export const getRandom5Colors = (): Color[] =>
-  new Array(5).fill(0).map(() => getRandomColorBuilder().getRandomColor())
+  new Array(5).fill(0).map(() => getRandomColor())
