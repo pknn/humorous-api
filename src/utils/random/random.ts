fc5844071e4cf7f69b27bf5c0a570893fddeac79
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2022-04-21 23:44:52 +0700

    feat: added implementation for random

--- src/utils/random/random.ts
+++ src/utils/random/random.ts
@@ -1,3 +1,5 @@
-export const getRandomNumber = (min: number, max: number) => 1
+export const getRandomNumber = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1)) + min
 
-export const getRandomElement = <T>(list: T[]): T => list[0] as T
+export const getRandomElement = <T>(list: T[]): T =>
+  list[getRandomNumber(0, list.length - 1)] as T
