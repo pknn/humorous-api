42d8d1dfff035aaf96b2061b0345cd44b8e9eeba
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2022-04-21 23:36:04 +0700

    feat: added RGBBuilder and HSLBuilder implementation

--- src/model/color/ColorBuilder.ts
+++ src/model/color/ColorBuilder.ts
@@ -1,6 +1,16 @@
+type ColorValue = number
 interface Color {}
-interface RGB extends Color {}
-interface HSL extends Color {}
+interface RGB extends Color {
+  red: ColorValue
+  green: ColorValue
+  blue: ColorValue
+}
+
+interface HSL extends Color {
+  hue: ColorValue
+  saturation: ColorValue
+  lightness: ColorValue
+}
 
 export abstract class ColorBuilder<T extends Color> {
   abstract getRandomColor(): T
@@ -8,12 +18,20 @@ export abstract class ColorBuilder<T extends Color> {
 
 export class RGBBuilder extends ColorBuilder<RGB> {
   getRandomColor(): RGB {
-    return {}
+    return {
+      red: 100,
+      green: 100,
+      blue: 100,
+    }
   }
 }
 
 export class HSLBuilder extends ColorBuilder<HSL> {
   getRandomColor(): HSL {
-    return {}
+    return {
+      hue: 200,
+      saturation: 100,
+      lightness: 100,
+    }
   }
 }
